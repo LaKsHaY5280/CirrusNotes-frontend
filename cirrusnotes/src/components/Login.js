3f5859d98c807819299567ea154b711f7653c551
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-05-24 18:28:14 +0530

    fixing

--- cirrusnotes/src/components/Login.js
+++ cirrusnotes/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const host = "https://cirrusnotes-backend.onrender.com/";
+const host = "https://cirrusnotes-backend.onrender.com";
 
 const Login = (props) => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
--- cirrusnotes/src/components/Signup.js
+++ cirrusnotes/src/components/Signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const host = "https://cirrusnotes-backend.onrender.com/";
+const host = "https://cirrusnotes-backend.onrender.com";
 
 const Signup = (props) => {
   const [credentials, setCredentials] = useState({
