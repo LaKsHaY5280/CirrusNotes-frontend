f30118ce56e8f6d5ad95e3b417bd29b9fad9095b
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-05-24 17:51:26 +0530

    for deployment

--- cirrusnotes/src/App.js
+++ cirrusnotes/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NoteState from "./context/notes/notesState";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+   
 
 function App() {
+  const [alert, setalert] = useState(null);
+  const toggleAlert = (type, msg) => {
+    setalert({
+      type: type,
+      msg: msg,
+    });
+
+    setTimeout(() => {
+      setalert(null);
+    }, 1250);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NoteState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Alert alert={alert} />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              key="general"
+              element={<Home toggleAlert={toggleAlert} />}
+            />
+            <Route
+              exact
+              path="/CirrusNotes"
+              key="general"
+              element={<Home toggleAlert={toggleAlert} />}
+            />
+            <Route
+              exact
+              path="/home"
+              key="general"
+              element={<Home toggleAlert={toggleAlert} />}
+            />
+            <Route
+              exact
+              path="/login"
+              key="general"
+              element={<Login toggleAlert={toggleAlert} />}
+            />
+            <Route
+              exact
+              path="/signup"
+              key="general"
+              element={<Signup toggleAlert={toggleAlert} />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </NoteState>
   );
 }
 
