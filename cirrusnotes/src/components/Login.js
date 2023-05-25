4642e41cef9bb1f51d5081367aa8aa05eee3207c
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-05-25 17:44:06 +0530

    fixing...

--- cirrusnotes/src/components/Login.js
+++ cirrusnotes/src/components/Login.js
@@ -8,43 +8,51 @@ const Login = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`${host}/api/v1/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: credentials.email,
-        password: credentials.password,
-      }),
-    });
-    const contentType = response.headers.get("Content-Type");
-    if (contentType && !contentType.includes("application/json")) {
-      console.error("Server returned non-JSON response:", response);
-      props.toggleAlert("warning", "Server error");
-      return;
-    }
-    const json = await response.json();
-    if (json.success) {
-      // Save the auth token and redirect
-      localStorage.setItem("token", json.authtoken);
-      props.toggleAlert("success", "Login complete");
-      navigate("/");
-    } else if (
-      response.status === 400 &&
-      json.error === "Email already exists"
-    ) {
-      props.toggleAlert("warning", "Email already exists");
-    } else if (json.errors) {
-      // Handle server-side validation errors
-      props.toggleAlert("warning", "Invalid credentials");
-      console.error("Server-side validation errors:", json.errors);
-      alert(
-        "Please correct the following errors:\n" +
-          json.errors.map((e) => e.msg).join("\n")
+    try {
+      const response = await fetch(`${host}/api/v1/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: credentials.email,
+          password: credentials.password,
+        }),
+      });
+      const contentType = response.headers.get("Content-Type");
+      if (contentType && !contentType.includes("application/json")) {
+        console.error("Server returned non-JSON response:", response);
+        props.toggleAlert("warning", "Server error");
+        return;
+      }
+      const json = await response.json();
+      if (json.success) {
+        // Save the auth token and redirect
+        localStorage.setItem("token", json.authtoken);
+        props.toggleAlert("success", "Login complete");
+        navigate("/");
+      } else if (
+        response.status === 400 &&
+        json.error === "Email already exists"
+      ) {
+        props.toggleAlert("warning", "Email already exists");
+      } else if (json.errors) {
+        // Handle server-side validation errors
+        props.toggleAlert("warning", "Invalid credentials");
+        console.error("Server-side validation errors:", json.errors);
+        alert(
+          "Please correct the following errors:\n" +
+            json.errors.map((e) => e.msg).join("\n")
+        );
+      } else {
+        props.toggleAlert("warning", "Invalid credentials");
+      }
+    } catch (error) {
+      console.error("Error during fetch request:", error);
+      props.toggleAlert(
+        "warning",
+        "An error occurred during the login process"
       );
-    } else {
-      props.toggleAlert("warning", "Invalid credentials");
     }
   };
   
